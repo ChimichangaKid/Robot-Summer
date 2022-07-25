d935c82cb9682c1bb876e7867c989575b6124fd1
Iosephub <80226506+Iosephub@users.noreply.github.com>
2022-07-25 15:11:02 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,24 @@
-#include <Arduino.h>
+#include<Arduino.h>
+#include<Servo.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#define BOMB_DETECT PB12
+int SERVO_PIN = A0;
+int servoAngle = 0;
+
+Servo myServo;
+
+void setup()
+{
+  pinMode(BOMB_DETECT, INPUT_PULLUP);
+  myServo.attach(SERVO_PIN);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  if (servoAngle>180 || digitalRead(BOMB_DETECT) == 0){
+    servoAngle = 0;
+  }
+  myServo.write(servoAngle);
+  delay(100);
+  servoAngle+=6;
 }

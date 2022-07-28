48bba58e7156ee3a6aae5c71b96a45d043a328d1
Iosephub <josephxkoh@gmail.com>
2022-07-28 15:01:38 -0700

    Working organized idol-detection.

--- include/idol-detection.h
+++ include/idol-detection.h
@@ -6,5 +6,10 @@ extern volatile unsigned long sonarStartMeasureMicroS;
 extern volatile unsigned long sonarEndMeasureMicroS;
 extern volatile int consecutiveDetectTally;
 
+#define CONSECUTIVE_DETECT_THRESHOLD 5
+#define IDOL_SEARCH_RADIUS_CM 15
+
+bool searchForIdol();
 void triggerSonar();
 void sonarMeasure();
+void sonarSetup();
--- src/idol-detection.cpp
+++ src/idol-detection.cpp
@@ -3,10 +3,8 @@
 #include <Wire.h>
 #include <Adafruit_SSD1306.h>
 
-#include "idol-detection.h"
-
-#define echoPin PB3
-#define trigPin PB4
+#include "../include/idol-detection.h"
+#include "../include/pins.h"
 
 volatile long duration; // variable for the duration of sound wave travel
 volatile int distance; // variable for the distance measurement
@@ -16,15 +14,36 @@ volatile unsigned long sonarStartMeasureMicroS = 0;
 volatile unsigned long sonarEndMeasureMicroS = 0;
 volatile int consecutiveDetectTally = 0;
 
+bool searchForIdol(){
+  if(consecutiveDetectTally>CONSECUTIVE_DETECT_THRESHOLD){
+    consecutiveDetectTally = 0;
+    return true;
+  }
+  else{
+    if (!sonarInProgress){
+      triggerSonar();
+      }
+    return false;
+  }
+}
+
+void sonarSetup(){
+  pinMode(TRIGGER_PIN, OUTPUT); // Sets the TRIGGER_PIN as an OUTPUT
+  pinMode(ECHO_PIN, INPUT); // Sets the echoPin as an INPUT
+
+  //Attach external IRQ pin to IRQ handler
+  attachInterrupt(digitalPinToInterrupt(ECHO_PIN), sonarMeasure, CHANGE);
+}
+
 void triggerSonar(){
   sonarInProgress=true;
-  // Clears the trigPin condition
-  digitalWrite(trigPin, LOW);
+  // Clears the TRIGGER_PIN condition
+  digitalWrite(TRIGGER_PIN, LOW);
   delayMicroseconds(2);
-  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
-  digitalWrite(trigPin, HIGH);
+  // Sets the TRIGGER_PIN HIGH (ACTIVE) for 10 microseconds
+  digitalWrite(TRIGGER_PIN, HIGH);
   delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
+  digitalWrite(TRIGGER_PIN, LOW);
 }
 
 void sonarMeasure(){
@@ -34,7 +53,7 @@ void sonarMeasure(){
   else {
     sonarEndMeasureMicroS = micros();
     distance = (sonarEndMeasureMicroS-sonarStartMeasureMicroS) * 0.034 / 2;
-    if (distance<30){
+    if (distance<IDOL_SEARCH_RADIUS_CM){
       consecutiveDetectTally++;
     }
     else{

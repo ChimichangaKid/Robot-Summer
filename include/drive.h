5ab1c8ddc570ae4e43eaa7d3ccf0f8af929a62e0
ChimichangaKid <realjaycegatsby@gmail.com>
2022-07-28 11:48:24 -0700

    [Tape Tracking] Used motor pins from pins.h and moved sensor pins to pins.h

--- include/drive.h
+++ include/drive.h
@@ -1,15 +1,8 @@
 #include <Arduino.h>
 
-#define MOTOR_PWM_OUTPUT_LEFT PA_8
-#define MOTOR_PWM_OUTPUT_LEFT_BACKWARDS PA_9
-#define MOTOR_PWM_OUTPUT_RIGHT PB_8
-#define MOTOR_PWM_OUTPUT_RIGHT_BACKWARDS PB_9
-
+#ifndef MOTOR_PWM_FREQUENCY_HZ
 #define MOTOR_PWM_FREQUENCY_HZ 100
-
-#define Relay2 PB12
-#define Relay3 PB13
-#define Relay1 PB14
+#endif
 
 void drive(int speedL, int speedR);
 void drive_setup();
--- src/drive.cpp
+++ src/drive.cpp
@@ -1,25 +1,26 @@
 #include <Arduino.h>
-#include "drive.h"
+#include "./../include/drive.h"
+#include "./../include/pins.h"
 
 void drive_setup(){
 /*
 May be repetitive to other setup function, DOUBLE CHECK
 */
 
-    pinMode(Relay1, OUTPUT);
-    pinMode(Relay2, OUTPUT);
-    pinMode(Relay3, OUTPUT);
+    pinMode(RELAY_CH1, OUTPUT);
+    pinMode(RELAY_CH2, OUTPUT);
+    pinMode(RELAY_CH3, OUTPUT);
 
-    pinMode(MOTOR_PWM_OUTPUT_LEFT, OUTPUT);
-    pinMode(MOTOR_PWM_OUTPUT_RIGHT, OUTPUT);
-    pinMode(MOTOR_PWM_OUTPUT_LEFT_BACKWARDS, OUTPUT);
-    pinMode(MOTOR_PWM_OUTPUT_RIGHT_BACKWARDS, OUTPUT);
+    pinMode(LEFT_DRIVE_A, OUTPUT);
+    pinMode(RIGHT_DRIVE_A, OUTPUT);
+    pinMode(LEFT_DRIVE_B, OUTPUT);
+    pinMode(RIGHT_DRIVE_B, OUTPUT);
 }
 
 void drive(int speedL, int speedR) {
-        digitalWrite(Relay1, 0);
-        digitalWrite(Relay2, 0);
-        digitalWrite(Relay3, 0);
+        digitalWrite(RELAY_CH1, 0);
+        digitalWrite(RELAY_CH2, 0);
+        digitalWrite(RELAY_CH3, 0);
 
         if(speedL > 100) speedL = 100;
         if(speedL < -100) speedL = -100;
@@ -27,27 +28,27 @@ void drive(int speedL, int speedR) {
         if(speedR < -100) speedR = -100;
 
         if (speedL >= 0) {
-                pwm_start(MOTOR_PWM_OUTPUT_LEFT_BACKWARDS, MOTOR_PWM_FREQUENCY_HZ, 0,
+                pwm_start(LEFT_DRIVE_B, MOTOR_PWM_FREQUENCY_HZ, 0,
                         TimerCompareFormat_t::PERCENT_COMPARE_FORMAT);
-                pwm_start(MOTOR_PWM_OUTPUT_LEFT, MOTOR_PWM_FREQUENCY_HZ, speedL,
+                pwm_start(LEFT_DRIVE_A, MOTOR_PWM_FREQUENCY_HZ, speedL,
                         TimerCompareFormat_t::PERCENT_COMPARE_FORMAT);
         }
         else {
-                pwm_start(MOTOR_PWM_OUTPUT_LEFT, MOTOR_PWM_FREQUENCY_HZ, 0,
+                pwm_start(LEFT_DRIVE_A, MOTOR_PWM_FREQUENCY_HZ, 0,
                         TimerCompareFormat_t::PERCENT_COMPARE_FORMAT);
-                pwm_start(MOTOR_PWM_OUTPUT_LEFT_BACKWARDS, MOTOR_PWM_FREQUENCY_HZ, -speedL,
+                pwm_start(LEFT_DRIVE_B, MOTOR_PWM_FREQUENCY_HZ, -speedL,
                         TimerCompareFormat_t::PERCENT_COMPARE_FORMAT);
         }
         if (speedR >= 0) {
-                pwm_start(MOTOR_PWM_OUTPUT_RIGHT_BACKWARDS, MOTOR_PWM_FREQUENCY_HZ, 0,
+                pwm_start(RIGHT_DRIVE_B, MOTOR_PWM_FREQUENCY_HZ, 0,
                         TimerCompareFormat_t::PERCENT_COMPARE_FORMAT);
-                pwm_start(MOTOR_PWM_OUTPUT_RIGHT, MOTOR_PWM_FREQUENCY_HZ, speedR,
+                pwm_start(RIGHT_DRIVE_A, MOTOR_PWM_FREQUENCY_HZ, speedR,
                         TimerCompareFormat_t::PERCENT_COMPARE_FORMAT);
         }
         else {
-                pwm_start(MOTOR_PWM_OUTPUT_RIGHT, MOTOR_PWM_FREQUENCY_HZ, 0,
+                pwm_start(RIGHT_DRIVE_A, MOTOR_PWM_FREQUENCY_HZ, 0,
                         TimerCompareFormat_t::PERCENT_COMPARE_FORMAT);
-                pwm_start(MOTOR_PWM_OUTPUT_RIGHT_BACKWARDS, MOTOR_PWM_FREQUENCY_HZ, -speedR,
+                pwm_start(RIGHT_DRIVE_B, MOTOR_PWM_FREQUENCY_HZ, -speedR,
                         TimerCompareFormat_t::PERCENT_COMPARE_FORMAT);
         }
 }

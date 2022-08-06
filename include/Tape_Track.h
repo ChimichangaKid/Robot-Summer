03e60c53cfd7b7b32c092bc645c794be56651251
Eric Fan <90150779+EricFan1110@users.noreply.github.com>
2022-08-06 11:42:23 -0700

    Update Tape_Track.h

--- include/Tape_Track.h
+++ include/Tape_Track.h
@@ -3,15 +3,13 @@
 #define LEFT_TAPE_THRESHOLD 700
 #define RIGHT_TAPE_THRESHOLD 700
 
-#define ON_DUTY_CYCLE 50
-#define TURN_DUTY_CYCLE 50
 #define Kp 0.4
 
 #define STRAIGHT 0
 #define TURN_RIGHT 1
 #define TURN_LEFT 2
 
-void TapeTrack();
+void TapeTrack(int speed);
 void setup_TapeTrack();
 void find_Tape();
 void find_Tape_Bomb();
